035660ddcdc827d55a68f15b650f24b8ccefb7de
Aniket-Work-2024 <aniketmondalwork@gmail.com>
2025-01-13 19:51:59 +0530

    :wrench: Taking user input in Question 1

--- Question 1/Employee.java
+++ Question 1/Employee.java
@@ -1,10 +1,21 @@
+import java.util.*;
+
 public class Employee {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         EmployeeDetails e = new EmployeeDetails();
-        e.getEname("Test_User");
-        e.setDesignation("Developer");
-        e.setSalary(50000);
-        e.getEmail("Test@user.com");
+        System.out.println("Enter employee name");
+        String name = sc.next();
+        System.out.println("Enter employee designation");
+        String disg = sc.next();
+        System.out.println("Enter employee sal");
+        int sal = sc.nextInt();
+        System.out.println("Enter employee email");
+        String email = sc.next();
+        e.getEname(name);
+        e.setDesignation(disg);
+        e.setSalary(sal);
+        e.getEmail(email);
         System.out.println(e.getEname()+" works as "+e.getDesignation()+" with email id "+e.getEmail()+" and earns "+e.getSalary()+" per annum");
     }
     
